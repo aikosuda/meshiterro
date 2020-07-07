1754ca9fa42805e58f39a2256ea166ab1eb6077b
vagrant <vagrant@localhost.localdomain>
2020-07-07 09:15:29 +0000

    [start]6-5

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -22,10 +22,17 @@ def show
 	@post_comment = PostComment.new
 end
 
+def destroy
+	@post_image = PostImage.find(params[:id])
+	@postimage.destroy
+	redirect_to post_images_path
 end
+
 
 private
 def post_image_params
 	params.require(:post_image).permit(:shop_name, :image, :caption)
+end
+
 end
 
